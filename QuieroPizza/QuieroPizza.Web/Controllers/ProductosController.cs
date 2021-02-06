e47d6a1116228ad3ff816c39823229f528c4dcfd
pc <pc@OSCAR>
2021-02-06 13:46:52 -0600

    Agregamos una lista de productos

---
+++ QuieroPizza/QuieroPizza.Web/Controllers/ProductosController.cs
@@ -0,0 +1,35 @@
+﻿using QuieroPizza.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuieroPizza.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.id = 1;
+            producto1.Descripcion = "Pizzas 6 quesos";
+
+            var producto2 = new ProductoModel();
+            producto2.id = 2;
+            producto2.Descripcion = "Pizzas 4 Estaciones";
+
+            var producto3 = new ProductoModel();
+            producto3.id = 3;
+            producto3.Descripcion = "Pizzas Jamon y Queso";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+
+            return View(listadeProductos);
+        }   
+    }
+}
---
+++ QuieroPizza/QuieroPizza.Web/Models/ProductoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuieroPizza.Web.Models
+{
+    public class ProductoModel
+    {
+     public int id { get; set; }
+        public string  Descripcion { get; set; }
+
+
+
+    }
+}
